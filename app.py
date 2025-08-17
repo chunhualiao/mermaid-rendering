709a4d8f5b23e8acc5cb2164770f3cf07b1990c0
Chunhua Liao <chunhualiao@gmail.com>
2025-08-16 22:39:54 -0700

    Update default mermaid code to mindmap example

--- app.py
+++ app.py
@@ -29,12 +29,23 @@ except RuntimeError as e:
 def index():
     """Display the main page with the form, including default example code."""
     default_mermaid_code = textwrap.dedent("""
-        graph TD
-            A[Start] --> B{Is it?};
-            B -- Yes --> C[OK];
-            C --> D[End];
-            B -- No --> E[Really?];
-            E --> C;
+        mindmap
+          root((mindmap))
+            Origins
+              Long history
+              ::icon(fa fa-book)
+              Popularisation
+                British popular psychology author Tony Buzan
+            Research
+              On effectiveness<br/>and features
+              On Automatic creation
+                Uses
+                    Creative techniques
+                    Strategic planning
+                    Argument mapping
+            Tools
+              Pen and paper
+              Mermaid
     """).strip()
 
     if renderer is None:
